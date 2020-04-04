1ad08a45997b17a4d2a9239b6b17d4fe26f5b134
Brian <Brian@DESKTOP-LTAFP54>
2020-04-05 02:55:40 +0900

    ~6강 (틀린곳 모두 처리)

--- Project_bangtal_hiddencatch.cpp
+++ Project_bangtal_hiddencatch.cpp
@@ -8,8 +8,8 @@
 SceneID scene1;
 ObjectID problem;
 
-ObjectID left1, left2;
-ObjectID right1, right2;
+ObjectID left1, left2, left3, left4, left5, left6, left7;
+ObjectID right1, right2, right3, right4, right5, right6, right7;
 
 //주어진 좌표(x,y)가 (cx, cy)를 중심으로 반지름 r인 정사각형 내부 -> true
 bool checkIn(int x, int y, int cx, int cy, int r) {
@@ -30,6 +30,36 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction) {
         else if (checkIn(x, y, 77, 499, 17) || checkIn(x, y, 695, 499, 17)) {
             showObject(left2);
             showObject(right2);
+        }
+
+        //3
+        else if (checkIn(x, y, 361, 430, 16) || checkIn(x, y, 979, 430, 16)) {
+            showObject(left3);
+            showObject(right3);
+        }
+
+        //4
+        else if (checkIn(x, y, 379, 106, 27) || checkIn(x, y, 997, 106, 27)) {
+            showObject(left4);
+            showObject(right4);
+        }
+
+        //5
+        else if (checkIn(x, y, 39, 203, 36)  || checkIn(x, y,657, 203, 36)) {
+            showObject(left5);
+            showObject(right5);
+        }
+
+        //6
+        else if (checkIn(x, y, 570, 369, 35) || checkIn(x, y, 1188, 369, 35)) {
+            showObject(left6);
+            showObject(right6);
+        }
+
+        //7
+        else if (checkIn(x, y, 298, 65, 13) || checkIn(x, y, 916, 65, 13)) {
+            showObject(left7);
+            showObject(right7);
         }
         else {
             endGame();
@@ -54,11 +84,46 @@ int main()
     left2 = createObject("체크 마크_왼2", "Images\\check.png");
     locateObject(left2, scene1, 77 - 25, 499 - 25);
 
+
+    left3 = createObject("체크 마크_왼3", "Images\\check.png");
+    locateObject(left3, scene1, 361 - 25, 430 - 25);
+
+    left4 = createObject("체크 마크_왼4", "Images\\check.png");
+    locateObject(left4, scene1, 379 - 25, 106 - 25);
+
+    left5 = createObject("체크 마크_왼5", "Images\\check.png");
+    locateObject(left5, scene1, 39 - 25, 203 - 25);
+
+    left6 = createObject("체크 마크_왼6", "Images\\check.png");
+    locateObject(left6, scene1, 570 - 25, 369 - 25);
+
+    left7 = createObject("체크 마크_왼7", "Images\\check.png");
+    locateObject(left7, scene1, 298 - 25, 65 - 25);
+
+
+
+
     right1 = createObject("체크 마크_오1", "Images\\check.png");
     locateObject(right1, scene1, 1164 - 25, 542 - 25);
 
     right2 = createObject("체크 마크_오2", "Images\\check.png");
     locateObject(right2, scene1, 695 - 25, 499 - 25);
+
+    right3 = createObject("체크 마크_오3", "Images\\check.png");
+    locateObject(right3, scene1, 979 - 25, 430 - 25);
+
+    right4 = createObject("체크 마크_오4", "Images\\check.png");
+    locateObject(right4, scene1, 997 - 25, 106 - 25);
+
+    right5 = createObject("체크 마크_오5", "Images\\check.png");
+    locateObject(right5, scene1, 657 - 25, 203 - 25);
+
+    right6 = createObject("체크 마크_오6", "Images\\check.png");
+    locateObject(right6, scene1, 1188 - 25, 369 - 25);
+
+    right7 = createObject("체크 마크_오7", "Images\\check.png");
+    locateObject(right7, scene1, 916 - 25, 65 - 25);
+
 
     showMessage("좌우에 틀린 곳을 찾아보세요.");
 
